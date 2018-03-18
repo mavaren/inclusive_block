3d9739223f39d550241625f2b95c0c57d0d99b85
Mattys Van Rensburg <mavaren@protonmail.com>
2018-03-18 16:12:44 +0000

    Basic version done

---
+++ content.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    replaceOnDocument(/(·[A-zÀ-ÿ]*·*)|(\.[A-zÀ-ÿ]*\.)/g, "");
+});
+
+function replaceOnDocument(pattern, string){
+  Array.from(document.querySelectorAll("body, body *:not(script):not(noscript):not(style)"))
+    .forEach(someNode => Array.from(someNode.childNodes)
+      .filter(childNode => childNode.nodeType == 3)
+      .forEach(textNode => textNode.textContent = textNode.textContent.replace(pattern, string)));
+}
+
