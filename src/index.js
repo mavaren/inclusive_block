3da6c122ef67b13b4ce3ddbec4834902734d65ca
Mattys Van Rensburg <mavaren@protonmail.com>
2019-07-24 12:14:41 +0200

    Useless function creation

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import $ from 'jquery'
 import purge from './purge.js'
 
 $( document ).ready(function() {
-    purgeDocument();
-});
-
-function purgeDocument(pattern, string){
   Array.from(document.querySelectorAll("body, body *:not(script):not(noscript):not(style)"))
     .forEach(someNode => Array.from(someNode.childNodes)
       .filter(childNode => childNode.nodeType == 3)
       .forEach(textNode => textNode.textContent = purge(textNode.textContent)));
-}
+});
