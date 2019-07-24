ee6bc67c836447009814222a6a57a0daed254a65
Mattys Van Rensburg <mavaren@protonmail.com>
2019-07-24 11:01:23 +0200

    Using NPM's jQuery

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: 'conteeent.js',
+    filename: 'content.js',
     path: path.resolve(__dirname, './')
   }
 };
