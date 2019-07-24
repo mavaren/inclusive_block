d5edb45ecb52ff8e5a6ddabb5a87fb94a556b309
Mattys Van Rensburg <mavaren@protonmail.com>
2019-07-24 11:23:55 +0200

    Import purge function from sources

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,15 +1,4 @@
-const MULTIDOT_PLURIAL_PATTERN = /([.·•][a-zA-Z\u00C0-\u017F]+[.·•]s)/g
-const S_SINGLEDOT_PLURIAL_PATTERN = /(s[.·•][a-zA-Z\u00C0-\u017F]+s)/g
-const SINGLEDOT_PLURIAL_PATTERN = /([.·•][a-zA-Z\u00C0-\u017F]+s)/g
-const SINGLEDOT_SINGULAR_PATTERN = /([.·•][a-zA-Z\u00C0-\u017F]+)/g
-
-function purge(string) {
-  return string
-    .replace(MULTIDOT_PLURIAL_PATTERN, 's')
-    .replace(S_SINGLEDOT_PLURIAL_PATTERN, 's')
-    .replace(SINGLEDOT_PLURIAL_PATTERN, 's')
-    .replace(SINGLEDOT_SINGULAR_PATTERN, '')
-}
+import purge from '../src/purge.js'
 
 test('True positives with ·', () => {
   expect(purge('les chef·fe·s')).toBe('les chefs')
