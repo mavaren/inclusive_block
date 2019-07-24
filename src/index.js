1354cf0cff80d7e44eddc1d1d13f651505e3f60a
Mattys Van Rensburg <mavaren@protonmail.com>
2019-07-24 12:39:43 +0200

    No unsafe assignment to innerHTML

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import $ from 'jquery'
 import purge from './purge.js'
 
 $( document ).ready(function() {
-  Array.from(document.querySelectorAll("body, body *:not(script):not(noscript):not(style)"))
-    .forEach(someNode => Array.from(someNode.childNodes)
-      .filter(childNode => childNode.nodeType == 3)
-      .forEach(textNode => textNode.textContent = purge(textNode.textContent)));
+  $('body, body *:not(script):not(noscript):not(style)').contents().filter(function() {
+    return this.nodeType == 3;
+  }).replaceWith(function() {
+      return purge(this.nodeValue)
+  });
 });
